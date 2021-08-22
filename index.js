7377e9ecf4d5e5532ea2d7853539646d65964903
Loren Cerri <lorencerri@gmail.com>
2021-08-22 03:29:19 -0400

    Clean up code

--- index.js
+++ index.js
@@ -1,91 +1,82 @@
 require("dotenv").config();
 
+// Discord.js
 const { Client, Intents, MessageEmbed } = require("discord.js");
-const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-});
+const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES];
+const client = new Client({ intents });
 
-const VirusTotalApi = require("virustotal-api");
-const virusTotal = new VirusTotalApi(process.env.VT_API_KEY);
+// Linkify (Scans text for URLs)
+const linkify = require("linkifyjs");
+const VirusTotalAPI = require("virustotal-api");
+const virusTotal = new VirusTotalAPI(process.env.VT_API_KEY);
 
-const { find } = require("linkifyjs");
-
-const createAnalysisEmbed = (result, malicious) => {
-    const embed = new MessageEmbed()
-        .setTitle(malicious ? "Malicious URL Detected" : `Analysis`)
-        .setColor(0x5865f2)
-        .setDescription(
-            `🔗 \`${result.url}\`\nScan Date: \`${result.scan_date}\``
-        )
-        .addField(
-            "Results",
-            `Positives: \`${result.positives}\`\nNegatives: \`${
-                result.total - result.positives
-            }\`\nSafe? \`${result.positives > 0 ? "No" : "Yes"}\``
-        );
-
-    if (malicious) {
-        embed.addField(
-            "Actions Taken",
-            "- Deleted Message\n- Added Muted Role"
-        );
-    }
-
-    return embed;
-};
-
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) return;
-
-    if (interaction.commandName === "ping") {
-        await interaction.reply("Pong!");
-    } else if (interaction.commandName === "analyze") {
-        const url = interaction.options.getString("url");
-        if (!url) return interaction.reply("Please provide a URL to analyze.");
-        const result = await virusTotal.urlReport(url, false, 1);
-        interaction.reply({ embeds: [createAnalysisEmbed(result)] });
-    }
-});
-
+// Message Handler
 client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
+    const senderId = message.author.id;
 
-    const results = find(message.content).filter((i) => i.type === "url");
-    if (results.length > 0) {
-        console.log(results);
+    // Match URLs in message
+    const urls = linkify
+        .find(message.content)
+        .filter((i) => i.type === "url")
+        .filter((i) => !i.value.startsWith("https://discord.com/")) // ignore discord links
+        .filter((i) => !i.value.endsWith(".png")) // ignore png images
+        .filter((i) => !i.value.endsWith(".mp4")); // ignore mp4 videos
 
-        for (var i = 0; i < results.length; i++) {
-            console.log(`Scanning ` + results[i].value);
-            var vt_result;
-            try {
-                vt_result = await virusTotal.urlReport(
-                    results[i].value,
-                    false,
-                    1
-                );
-            } catch (e) {
-                return console.log("Rate Limit");
-            }
-            if (vt_result && vt_result.positives > 0) {
-                message.delete();
-                const mutedRole = message.guild.roles.cache.find(
-                    (r) => r.name === "Muted"
-                );
-                if (mutedRole) message.member.roles.add(mutedRole);
-                if (message.guild.id === "343572980351107077") {
-                    message.guild.channels.cache
-                        .get("398491167005868043")
-                        .send({
-                            embeds: [createAnalysisEmbed(vt_result, true)],
-                        });
-                }
-            }
+    let action_taken = false;
+
+    for (var i = 0; i < urls.length; i++) {
+        if (action_taken) break;
+
+        const url = urls[i].value;
+        console.log(`Scanning ${url}`);
+
+        // Catch Rate Limit Errors
+        var result;
+        try {
+            result = await virusTotal.urlReport(url, false, 1);
+        } catch (err) {}
+
+        // Positive Results
+        if (result && result.positives > 0) {
+            action_taken = true;
+
+            // Delete Message
+            message.delete();
+
+            // Add Muted Role
+            const mutedRole = message.guild.roles.cache.find(
+                (r) => r.name === "Muted"
+            );
+            if (mutedRole) message.member.roles.add(mutedRole);
+
+            // Create Alert
+            const embed = new MessageEmbed()
+                .setTitle("Malicious URL Detected")
+                .setColor(0x5865f2)
+                .setURL(result.permalink)
+                .setDescription(
+                    `🔗 \`${url}\`\nScan Date: \`${result.scan_date}\``
+                )
+                .addField(
+                    "Positives",
+                    `\`${result.positives}/${result.total}\``,
+                    true
+                )
+                .addField("Sender", `<@${senderId}> \`(${senderId})\``, true);
+
+            // Send Alert
+            message.guild.channels.cache
+                .get("398491167005868043")
+                .send({ embeds: [embed] });
         }
     }
 });
 
+// Ready Event
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.username}`);
 });
 
+// Login
 client.login(process.env.DISCORD_TOKEN);
