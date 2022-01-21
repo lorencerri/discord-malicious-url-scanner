b2b500c44d2eb9fc7fbc4bf2d1f25530a4d44e01
Loren Cerri <lorencerri@gmail.com>
2022-01-21 18:22:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 
 // Discord.js
 const {
-    Client,
-    Intents,
-    MessageEmbed,
-    MessageButton,
-    MessageActionRow,
+	Client,
+	Intents,
+	MessageEmbed,
+	MessageButton,
+	MessageActionRow,
 } = require("discord.js");
 const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES];
 const client = new Client({ intents });
@@ -17,122 +17,127 @@ const virusTotal = new VirusTotalAPI(process.env.VT_API_KEY);
 
 // Button Handler
 client.on("interactionCreate", (interaction) => {
-    if (!interaction.isButton()) return;
+	if (!interaction.isButton()) return;
 
-    // Handle descriptive buttons
-    if (interaction.customId === "_") {
-        interaction.reply({
-            ephemeral: true,
-            content: "*This button just displays information...*",
-        });
-    }
+	// Handle descriptive buttons
+	if (interaction.customId === "_") {
+		interaction.reply({
+			ephemeral: true,
+			content: "*This button just displays information...*",
+		});
+	}
 
-    const executor = interaction.member;
-    const action = interaction.customId.split("_")[0];
+	const executor = interaction.member;
+	const action = interaction.customId.split("_")[0];
 
-    if (action === "ban") {
-        if (!executor.roles.cache.find((r) => r.name === "Moderator"))
-            return interaction.reply({
-                ephemeral: true,
-                content: "You do not have permission to use this button.",
-            });
+	if (action === "ban") {
+		if (!executor.roles.cache.find((r) => r.name === "Moderator"))
+			return interaction.reply({
+				ephemeral: true,
+				content: "You do not have permission to use this button.",
+			});
 
-        const sender = interaction.member.guild.members.cache.get(
-            interaction.customId.split("_")[1]
-        );
+		const sender = interaction.member.guild.members.cache.get(
+			interaction.customId.split("_")[1]
+		);
 
-        sender.ban({
-            days: 1,
-            reason: `Banned by ${executor.user.tag} (manual from malicious link)`,
-        });
+		sender.ban({
+			days: 1,
+			reason: `Banned by ${executor.user.tag} (manual from malicious link)`,
+		});
 
-        const row = new MessageActionRow().addComponents(
-            new MessageButton()
-                .setCustomId("_")
-                .setLabel(`Successfully banned by ${executor.user.tag}!`)
-                .setStyle("SUCCESS")
-        );
+		const row = new MessageActionRow().addComponents(
+			new MessageButton()
+				.setCustomId("_")
+				.setLabel(`Successfully banned by ${executor.user.tag}!`)
+				.setStyle("SUCCESS")
+		);
 
-        interaction.update({ components: [row] });
-    } else if (action === "cancel") interaction.update({ components: [] });
+		interaction.update({ components: [row] });
+	} else if (action === "cancel") interaction.update({ components: [] });
 });
+
+const cache = new Set();
 
 // Message Handler
 client.on("messageCreate", async (message) => {
-    if (message.author.bot) return;
-    const senderId = message.author.id;
+	if (message.author.bot) return;
+	const senderId = message.author.id;
 
-    // Match URLs in message
-    const urls = linkify
-        .find(message.content)
-        .filter((i) => i.type === "url")
-        .filter((i) => !i.value.startsWith("https://discord.com/")) // ignore discord links
-        .filter((i) => !i.value.endsWith(".png")) // ignore png images
-        .filter((i) => !i.value.endsWith(".mp4")); // ignore mp4 videos
-    //.filter((i) => i.value.startsWith('http://') || i.value.startsWith('https://'))
-    // ignoring certain files isn't necessary, it's only to ensure VirusTotal ratelimits aren't met
+	// Match URLs in message
+	const urls = linkify
+		.find(message.content)
+		.filter((i) => i.type === "url")
+		.filter((i) => !i.value.startsWith("https://discord.com/")) // ignore discord links
+		.filter((i) => !i.value.endsWith(".png")) // ignore png images
+		.filter((i) => !i.value.endsWith(".mp4")); // ignore mp4 videos
+	//.filter((i) => i.value.startsWith('http://') || i.value.startsWith('https://'))
+	// ignoring certain files isn't necessary, it's only to ensure VirusTotal ratelimits aren't met
 
-    for (var i = 0; i < urls.length; i++) {
-        const url = urls[i].value;
-        console.log(`Scanning ${url}`);
+	for (var i = 0; i < urls.length; i++) {
+		const url = urls[i].value;
+		console.log(`Scanning ${url}`);
 
-        // Catch Rate Limit Errors
-        var result;
-        try {
-            result = await virusTotal.urlReport(url, false, 1);
-        } catch (err) {}
+		if (cache.has(`${senderId}-${url}`)) return;
+		else cache.add(`${senderId}-${url}`);
 
-        // Positive Results (greater than 1 due to false positives)
-        if (result && result.positives > 1) {
-            // Delete Message
-            message.delete();
+		// Catch Rate Limit Errors
+		var result;
+		try {
+			result = await virusTotal.urlReport(url, false, 1);
+		} catch (err) { }
 
-            // Add Muted Role
-            const mutedRole = message.guild.roles.cache.find(
-                (role) => role.name === "Muted"
-            );
-            message.member.roles.add(mutedRole);
+		// Positive Results (greater than 1 due to false positives)
+		if (result && result.positives > 1) {
+			// Delete Message
+			message.delete();
 
-            // Create Alert
-            const embed = new MessageEmbed()
-                .setTitle("Malicious URL Detected")
-                .setColor(0x5865f2)
-                .setURL(result.permalink)
-                .setDescription(
-                    `🔗 \`${url}\`\nScan Date: \`${result.scan_date}\``
-                )
-                .addField(
-                    "Positives",
-                    `\`${result.positives}/${result.total}\``,
-                    true
-                )
-                .addField("Sender", `<@${senderId}> \`(${senderId})\``, true);
+			// Add Muted Role
+			const mutedRole = message.guild.roles.cache.find(
+				(role) => role.name === "Muted"
+			);
+			message.member.roles.add(mutedRole);
 
-            // Add buttons
-            const row = new MessageActionRow().addComponents(
-                new MessageButton()
-                    .setCustomId(`ban_${senderId}`)
-                    .setLabel("Ban")
-                    .setStyle("DANGER"),
-                new MessageButton()
-                    .setCustomId("cancel")
-                    .setLabel("Cancel")
-                    .setStyle("PRIMARY")
-            );
+			// Create Alert
+			const embed = new MessageEmbed()
+				.setTitle("Malicious URL Detected")
+				.setColor(0x5865f2)
+				.setURL(result.permalink)
+				.setDescription(
+					`🔗 \`${url}\`\nScan Date: \`${result.scan_date}\``
+				)
+				.addField(
+					"Positives",
+					`\`${result.positives}/${result.total}\``,
+					true
+				)
+				.addField("Sender", `<@${senderId}> \`(${senderId})\``, true);
 
-            // Send Alert
-            message.guild.channels.cache
-                .get("873249087514894366")
-                .send({ embeds: [embed], components: [row] });
+			// Add buttons
+			const row = new MessageActionRow().addComponents(
+				new MessageButton()
+					.setCustomId(`ban_${senderId}`)
+					.setLabel("Ban")
+					.setStyle("DANGER"),
+				new MessageButton()
+					.setCustomId("cancel")
+					.setLabel("Cancel")
+					.setStyle("PRIMARY")
+			);
 
-            break;
-        }
-    }
+			// Send Alert
+			message.guild.channels.cache
+				.get("873249087514894366")
+				.send({ embeds: [embed], components: [row] });
+
+			break;
+		}
+	}
 });
 
 // Ready Event
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.username}`);
+	console.log(`Logged in as ${client.user.username}`);
 });
 
 // Login
